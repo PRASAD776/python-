d36b3682fc5d46bbcbfb577651b9d04132b92c3e
P R A S A D <126767565+PRASAD776@users.noreply.github.com>
2024-10-15 19:05:12 +0530

    initial commit

--- atm.py
+++ atm.py
@@ -6,26 +6,24 @@ print("1. English")
 print("2. Hindi")
 print("3. kannada")
 language= int(input("Enter your choice(1/2/3)  "))
-
-if language== 1:
-  
+if language>3:
+    print("invalid language choice")
+elif language==2:
+    print("hindi")
+elif language==3:
+    print("kannada")
+else:
+    print("english")
     pin = int(input("Enter your 4-digit PIN: "))
-    # print("enter the digit")
-    # pin=int(input())
-    
     if pin == atmpin:
         print("PIN verified.")
-        
         # print("Welcome to the ATM!")
         print("1. Check Balance")
         print("2. Deposit")
         print("3. Withdraw")
-
         choice = int(input("Enter your choice (1/2/3) "))
-
         if choice == 1:
             print("your balance",balance)
-        
         elif choice == 2:
             deposit = int(input("Enter amount to deposit: "))
             if deposit > 0:
@@ -34,7 +32,6 @@ if language== 1:
                 print("your new balance is:",balance)
             else:
                 print("Invalid amount entered.")
-            
         elif choice == 3:
             withdraw_amount = int(input("Enter amount to withdraw: "))
             if  withdraw_amount <= balance:
@@ -45,11 +42,8 @@ if language== 1:
                 print("Insufficient balance.")
             else:
                 print("Invalid amount entered.")
-        
         else:
             print("Invalid choice.")
-        
         print("Thank you for using the ATM!")
-    
     else:
         print("Incorrect PIN. Please try again.")
