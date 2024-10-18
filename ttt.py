83dac0db054041eae7f7161b65076f3c0b980fc1
P R A S A D <126767565+PRASAD776@users.noreply.github.com>
2024-10-18 22:11:41 +0530

    initial commit

--- ttt.py
+++ ttt.py
@@ -1,15 +1,17 @@
-class calculator():
-    def __init__(self):
-        self.a=10
-        self.b=20
-    def disp(self):
-        x=100
-        y=200
-        z=x+y
-        print(z)
-c1=calculator()
-print(c1.a)
-print(c1.b)
-c1.disp()
-
-
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this 
+grey=1
+blue=2
+she=int(input("ask she is \n1.online \n2.offline\n-->"))
+if she ==1:
+    tick=int(input("which tick \n1.grey\n2.blue\n-->"))
+    print("she is online")
+    if tick==grey:
+        print("ignoring ur mesg")
+    elif tick==blue:
+        print("seen ur msg")
+elif she ==2:
+    print("mesg sent")
+    print("offline ")
+else:
+    print("mesg not sent")
