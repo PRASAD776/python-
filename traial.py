e04dfe40c7e2022b52062db42fd874d391429f4a
P R A S A D <126767565+PRASAD776@users.noreply.github.com>
2024-10-18 21:40:00 +0530

    initial commit

--- traial.py
+++ traial.py
@@ -1,3 +1,44 @@
-print("cghoose the car :\n 1.car\n 2.bike\n")
-days=int(input())
-print(days*100)
+# print("cghoose the car :\n 1.car\n 2.bike\n")
+# days=int(input())
+# print(days*100)
+# print(True>(False-1))
+# print(5>1)
+# print(5*5>4**1)
+# print(False>True)
+# print((3+2+2>=1+4))
+# print(False or False and True or False)
+# print((6*5)==30 or ((7*21)!=(3**3)))
+# print(not((True and False and False)or(3>-1)))
+# print(((45+5)!=(90-45))and((2**5)<(5**3))and False)
+# a=5
+# print(a)
+# a=10
+# print(a)
+# b=10
+# print(b)
+# c=b
+# print(c)
+# b=200
+# print(a)
+# print(b)
+# print(c)
+
+# a=10
+# b=2
+# c=3
+# c*=a+b
+# print(c)
+a=10
+b=10
+print("before id's:i ")
+print("id of a",id(a))
+print("id of b",id(b))
+print(a is b)
+c=20
+b+=c
+print("after id's:")
+print("id of a",id(a))
+print("id of b",id(b))
+print("id of c",id(c))
+print(a is not b)
+print(c is b)
---
+++ ttt.py
@@ -0,0 +1,15 @@
+class calculator():
+    def __init__(self):
+        self.a=10
+        self.b=20
+    def disp(self):
+        x=100
+        y=200
+        z=x+y
+        print(z)
+c1=calculator()
+print(c1.a)
+print(c1.b)
+c1.disp()
+
+
